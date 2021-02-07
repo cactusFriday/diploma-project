8b9f78bb4b88ba86651188105c2b1f35fa4f2bd1
cactusFriday <gnatyuk.alexander@mail.ru>
2021-02-07 16:37:37 +0300

    added face detection via face_recognition lib

--- video_stream/cam_handle.py
+++ video_stream/cam_handle.py
@@ -1,6 +1,7 @@
 import cv2, imutils
-from imutils.video import FPS
+# from imutils.video import FPS
 import datetime as dt
+import face_recognition
 
 class VideoCamera(object):
     def __init__(self):
@@ -8,10 +9,24 @@ class VideoCamera(object):
     def __del__(self):
         self.video.release()
     def get_frame(self):
-        ret, frame = self.video.read()
-        frame = cv2.flip(frame,1)
-        frame = imutils.resize(frame, width=600)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        ret, jpeg = cv2.imencode('.jpg', gray)
+        self.ret, self.frame = self.video.read()
+        self.frame = cv2.flip(self.frame,1)
+        self.frame = imutils.resize(self.frame, width=600)
+        self.face_detect()
+        # gray = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+        ret, jpeg = cv2.imencode('.jpg', self.frame)
         # self.fps = dt.time() - self.fps
-        return (jpeg.tobytes(), self.fps)
+        return jpeg.tobytes()
+
+    def face_detect(self):
+        small_frame = cv2.resize(self.frame, (0, 0), fx=0.25, fy=0.25)
+        rgb_small_frame = small_frame[:, :, ::-1]
+        face_locations = face_recognition.face_locations(rgb_small_frame)
+        for (top, right, bottom, left) in face_locations:
+            top *= 4
+            right *= 4
+            bottom *= 4
+            left *= 4
+            cv2.rectangle(self.frame, (left, top), (right, bottom), (0, 255, 0), 2)
+        # print(f'coords: {self.detections[1][0]}|{self.detections[1][1]}|{self.detections[1][2]}|{self.detections[1][3]}')
+        # print(f'Confidence: {self.detections[1][4]}')
--- video_stream/views.py
+++ video_stream/views.py
@@ -6,12 +6,26 @@ from django.views import generic
 from django.utils import timezone
 from django.views.decorators import gzip
 import cv2
-
+import numpy as np
+# import dlib
+import face_detection
+# from face_detection.retinaface.tensorrt_wrap import TensorRTRetinaFace
+# from face_detection.retinaface import RetinaNetResNet50
 from .cam_handle import VideoCamera
 
 def gen(camera):
     while True:
-        frame, fps = camera.get_frame()
+        frame = camera.get_frame()
+        # img = np.ndarray(frame)
+        # inference_imshape =(480, 640) # Input to the CNN
+        # input_imshape = (1080, 1920) # Input for original video source
+        # detector = RetinaNetResNet50()
+        # boxes, landmarks, scores = detector.detect(image=img)
+        # print("Number of faces detected: {}".format(len(dets)))
+        # for i, d in enumerate(dets):
+        #     print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} Confidence: {}".format(
+        #         i, d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
+
         # print(fps)
         yield(b'--frame\r\n'
         b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
